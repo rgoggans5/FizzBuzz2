f30403b8fa0aad468161f3db9c50e8ad6f80a0a9
rageangoggans <rageangoggans@gmail.com>
2015-12-02 19:26:48 -0500

    FizzBuzz2

---
+++ FizzBuzz2/FizzBuzz2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {       //print nums from 1-100
+            for (int i = 1; i <= 100; i++)
+            //for multiple of 5&3, fizzbuzz
+            {
+                if (i % 15 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                //for every multiple of 3,fizz
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+
+                //for every multiple of 5,buzz
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                //prints next number
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+
+
+        }
+    }
+}
---
+++ FizzBuzz2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//print nums from 1-100
+            for (int i = 1; i <=100; i++)
+            {//for every multiple of 3,fizz
+                if (i%15==0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if(i % 5 == 0)
+                    {
+                    Console.WriteLine("buzz");
+                    }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+         
+         //for every multiple of 5,buzz
+         //for multiple of 5&3, fizzbuzz
+
+        }
+    }
+}
